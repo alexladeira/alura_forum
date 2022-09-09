a0b6907068cd904831e3a62bc0f6c2d2c2741eb1
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-09-09 01:25:29 -0300

    adding repository test - database configuration

--- src/test/java/br/com/alura/alura_forum/curso/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/alura_forum/curso/CursoRepositoryTest.java
@@ -1,17 +1,30 @@
 package br.com.alura.alura_forum.curso;
 
 import br.com.alura.alura_forum.model.Curso;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+//spring default database test to a memory database, use this configuration to avoid that
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private Curso curso = createCurso();
 
     @Test
     void should_loadCurso_when_findByNomeFoundCurso() {
@@ -26,5 +39,23 @@ class CursoRepositoryTest {
         String nomeCurso = "JPA";
         Curso curso = repository.findByNome(nomeCurso);
         assertNull(curso);
+    }
+
+    @BeforeEach
+    void setUp() {
+        curso = entityManager.persist(curso);
+    }
+
+    @AfterEach
+    void tearDown() {
+        entityManager.remove(curso);
+    }
+
+    private Curso createCurso() {
+        Curso curso = new Curso();
+        curso.setNome("HTML 5");
+        curso.setCategoria("Programacao");
+
+        return curso;
     }
 }
